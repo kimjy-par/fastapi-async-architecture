e49e42eb18a820ccad3c76379cea23b55f9dd8fa
kimjy-par <kimjy.par@gmail.com>
2024-08-30 15:50:01 +0900

    Refac: remove unnecessary initialization

--- app/repositories/post_repository.py
+++ app/repositories/post_repository.py
@@ -1,7 +1,6 @@
 from contextlib import AbstractAsyncContextManager
 from typing import Callable
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import joinedload
 from sqlalchemy.future import select
 from app.models.post import Post
 from app.models.user import User
@@ -22,7 +21,8 @@ class PostRepository(BaseRepository):
         async with self.session_factory() as session:
             user = await session.execute(select(User).where(User.id == user_id))
             user = user.scalars().first()
-
+            if not user:
+                raise NotFoundError()
             model = self.model(**schema.model_dump())
             model.user = user
 
